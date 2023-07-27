487d36b154f709d3b0b34bc73842d3e0d2219e37
SuprajaKonchada <supraja.konchada@cognine.com>
2023-07-27 14:48:16 +0530

    Application

---
+++ UserRegistrationAndLoginPage/Application/ForgotPassword.aspx.cs
@@ -0,0 +1,52 @@
+﻿using BusinessModel;
+using Literals;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Application
+{
+    public partial class ForgotPassword : System.Web.UI.Page
+
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        /// <summary>
+        /// reset's the password of the user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            BALAuthentication BALAuth = new BALAuthentication();
+            string username = txtUsername.Text.Trim().ToLower();
+            string newpassword = txtPassword.Text;
+            string confirmPassword = txtConfirmpwd.Text;
+            if(BALAuth.IsPasswordMatch(newpassword, confirmPassword)==false)
+            {
+                lblMessage.Text = StringLiterals.NotMatch;
+                lblMessage.Visible = true;
+                return;
+            }
+            string encryptedPassword = BALAuth.EncryptPassword(newpassword);
+            try
+            {
+                BALAuth.UpdateInfo(username, encryptedPassword);
+                Response.Redirect(StringLiterals.ToLogin);
+            }
+            catch (SqlException ex)
+            {
+                // Handle specific SQL Server exceptions here
+                lblMessage.Text = StringLiterals.DataBaseError + ex.Message;
+                lblMessage.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                // Handle other general exceptions here
+                lblMessage.Text = StringLiterals.UnexpectedError + ex.Message;
+                lblMessage.Visible = true;
+            }
+        }
+    }
+}
---
+++ UserRegistrationAndLoginPage/DataLayer/DataBaseOperations.cs
@@ -0,0 +1,50 @@
+﻿using Literals;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DataBaseOperations
+    {
+        /// <summary>
+        /// adds the information into database
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="email"></param>
+        /// <param name="encryptedPassword"></param>
+        public void AddInfoToDataBase(string username, string email, string encryptedPassword)
+        {
+            string ConnectionStr = ConfigurationManager.ConnectionStrings["Data"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(ConnectionStr))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(StringLiterals.InsertStatement, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@EmailID", email);
+                    command.Parameters.AddWithValue("@Password", encryptedPassword);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        /// <summary>
+        /// updates information into database
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="encryptedPassword"></param>
+        public void UpdateInfoToDataBase(string username, string encryptedPassword)
+        {
+            string ConnectionStr = ConfigurationManager.ConnectionStrings["Data"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(ConnectionStr))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(StringLiterals.UpdateStatement, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", encryptedPassword);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
